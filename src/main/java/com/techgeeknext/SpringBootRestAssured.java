cdace7a279c1afc4a947bf795a4add9312618db1
TechGeekNeXt <techgeeknext@gmail.com>
2022-02-20 14:51:17 +0530

    tgn001

---
+++ src/main/java/com/techgeeknext/SpringBootRestAssured.java
@@ -0,0 +1,13 @@
+package com.techgeeknext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestAssured {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestAssured.class, args);
+	}
+
+}
